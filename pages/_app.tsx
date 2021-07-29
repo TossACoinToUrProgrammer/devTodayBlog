91db90beeb5798e0d7d7c993bcab1ec701fb0283
Arlen Manasov <59771669+TossACoinToUrProgrammer@users.noreply.github.com>
2021-07-30 01:10:41 +0600

    Add preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import React from "react"
 import "../styles/globals.css"
 import { wrapper } from "../store/index"
-import React from "react"
+import NextNprogress from 'nextjs-progressbar';
+
 
 const WrappedApp: React.FC<any> = ({ Component, pageProps }): any => {
-  return <Component {...pageProps} />
+  return <>
+    <NextNprogress
+      color="#c03c85c6"
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={7}
+      showOnShallow={true}
+    />
+    <Component {...pageProps} /></>
 };
 
 export default wrapper.withRedux(WrappedApp)
